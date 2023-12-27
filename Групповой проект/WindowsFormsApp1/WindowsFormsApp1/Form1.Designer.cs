1bc8ba45cb6a19715bcd257102d8a82f27b3cda2
alina <annadura@gmail.com>
2023-12-27 17:22:56 +0300

    Add shifr 5

--- Групповой проект/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Групповой проект/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -49,18 +49,20 @@ namespace WindowsFormsApp1
             // choice
             // 
             this.choice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.choice.Location = new System.Drawing.Point(294, 9);
+            this.choice.Location = new System.Drawing.Point(441, 14);
+            this.choice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.choice.Name = "choice";
-            this.choice.Size = new System.Drawing.Size(291, 40);
+            this.choice.Size = new System.Drawing.Size(436, 62);
             this.choice.TabIndex = 0;
             this.choice.Text = "Выберите шифр";
             // 
             // Input_label
             // 
             this.Input_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Input_label.Location = new System.Drawing.Point(10, 150);
+            this.Input_label.Location = new System.Drawing.Point(15, 234);
+            this.Input_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Input_label.Name = "Input_label";
-            this.Input_label.Size = new System.Drawing.Size(260, 40);
+            this.Input_label.Size = new System.Drawing.Size(390, 62);
             this.Input_label.TabIndex = 1;
             this.Input_label.Text = "Введите текст";
             // 
@@ -68,9 +70,10 @@ namespace WindowsFormsApp1
             // 
             this.Shifrator.Enabled = false;
             this.Shifrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifrator.Location = new System.Drawing.Point(35, 650);
+            this.Shifrator.Location = new System.Drawing.Point(52, 1016);
+            this.Shifrator.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifrator.Name = "Shifrator";
-            this.Shifrator.Size = new System.Drawing.Size(300, 50);
+            this.Shifrator.Size = new System.Drawing.Size(450, 78);
             this.Shifrator.TabIndex = 2;
             this.Shifrator.Text = "Зашифровать";
             this.Shifrator.UseVisualStyleBackColor = true;
@@ -80,9 +83,10 @@ namespace WindowsFormsApp1
             // 
             this.Deshifrator.Enabled = false;
             this.Deshifrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Deshifrator.Location = new System.Drawing.Point(547, 650);
+            this.Deshifrator.Location = new System.Drawing.Point(820, 1016);
+            this.Deshifrator.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Deshifrator.Name = "Deshifrator";
-            this.Deshifrator.Size = new System.Drawing.Size(300, 50);
+            this.Deshifrator.Size = new System.Drawing.Size(450, 78);
             this.Deshifrator.TabIndex = 3;
             this.Deshifrator.Text = "Расшифровать";
             this.Deshifrator.UseVisualStyleBackColor = true;
@@ -91,31 +95,34 @@ namespace WindowsFormsApp1
             // Output_label
             // 
             this.Output_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Output_label.Location = new System.Drawing.Point(10, 380);
+            this.Output_label.Location = new System.Drawing.Point(15, 594);
+            this.Output_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Output_label.Name = "Output_label";
-            this.Output_label.Size = new System.Drawing.Size(197, 40);
+            this.Output_label.Size = new System.Drawing.Size(296, 62);
             this.Output_label.TabIndex = 4;
             this.Output_label.Text = "Результат";
             // 
             // Input
             // 
             this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Input.Location = new System.Drawing.Point(10, 200);
+            this.Input.Location = new System.Drawing.Point(15, 312);
+            this.Input.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Input.Multiline = true;
             this.Input.Name = "Input";
             this.Input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Input.Size = new System.Drawing.Size(850, 150);
+            this.Input.Size = new System.Drawing.Size(1273, 232);
             this.Input.TabIndex = 5;
             this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
             // 
             // Output
             // 
             this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Output.Location = new System.Drawing.Point(10, 425);
+            this.Output.Location = new System.Drawing.Point(15, 664);
+            this.Output.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Output.Multiline = true;
             this.Output.Name = "Output";
             this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Output.Size = new System.Drawing.Size(850, 150);
+            this.Output.Size = new System.Drawing.Size(1273, 232);
             this.Output.TabIndex = 6;
             this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
             // 
@@ -123,9 +130,10 @@ namespace WindowsFormsApp1
             // 
             this.Shifr1.Checked = true;
             this.Shifr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifr1.Location = new System.Drawing.Point(15, 50);
+            this.Shifr1.Location = new System.Drawing.Point(22, 78);
+            this.Shifr1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifr1.Name = "Shifr1";
-            this.Shifr1.Size = new System.Drawing.Size(209, 40);
+            this.Shifr1.Size = new System.Drawing.Size(314, 62);
             this.Shifr1.TabIndex = 7;
             this.Shifr1.TabStop = true;
             this.Shifr1.Text = "шифр Атбаш";
@@ -134,9 +142,10 @@ namespace WindowsFormsApp1
             // Shifr2
             // 
             this.Shifr2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifr2.Location = new System.Drawing.Point(15, 100);
+            this.Shifr2.Location = new System.Drawing.Point(22, 156);
+            this.Shifr2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifr2.Name = "Shifr2";
-            this.Shifr2.Size = new System.Drawing.Size(250, 40);
+            this.Shifr2.Size = new System.Drawing.Size(375, 62);
             this.Shifr2.TabIndex = 8;
             this.Shifr2.Text = "шифр XOR";
             this.Shifr2.UseVisualStyleBackColor = true;
@@ -144,9 +153,10 @@ namespace WindowsFormsApp1
             // Shifr3
             // 
             this.Shifr3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifr3.Location = new System.Drawing.Point(300, 50);
+            this.Shifr3.Location = new System.Drawing.Point(450, 78);
+            this.Shifr3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifr3.Name = "Shifr3";
-            this.Shifr3.Size = new System.Drawing.Size(498, 40);
+            this.Shifr3.Size = new System.Drawing.Size(747, 62);
             this.Shifr3.TabIndex = 9;
             this.Shifr3.Text = "модифицированная азбука Морзе";
             this.Shifr3.UseVisualStyleBackColor = true;
@@ -154,9 +164,10 @@ namespace WindowsFormsApp1
             // Shifr4
             // 
             this.Shifr4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifr4.Location = new System.Drawing.Point(300, 100);
+            this.Shifr4.Location = new System.Drawing.Point(450, 156);
+            this.Shifr4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifr4.Name = "Shifr4";
-            this.Shifr4.Size = new System.Drawing.Size(214, 40);
+            this.Shifr4.Size = new System.Drawing.Size(321, 62);
             this.Shifr4.TabIndex = 10;
             this.Shifr4.Text = "шифр Цезаря";
             this.Shifr4.UseVisualStyleBackColor = true;
@@ -164,9 +175,10 @@ namespace WindowsFormsApp1
             // Shifr5
             // 
             this.Shifr5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Shifr5.Location = new System.Drawing.Point(566, 100);
+            this.Shifr5.Location = new System.Drawing.Point(849, 156);
+            this.Shifr5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Shifr5.Name = "Shifr5";
-            this.Shifr5.Size = new System.Drawing.Size(253, 40);
+            this.Shifr5.Size = new System.Drawing.Size(380, 62);
             this.Shifr5.TabIndex = 11;
             this.Shifr5.Text = "шифр Полибия";
             this.Shifr5.UseVisualStyleBackColor = true;
@@ -175,9 +187,10 @@ namespace WindowsFormsApp1
             // 
             this.Clean_input.Enabled = false;
             this.Clean_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Clean_input.Location = new System.Drawing.Point(866, 241);
+            this.Clean_input.Location = new System.Drawing.Point(1299, 377);
+            this.Clean_input.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Clean_input.Name = "Clean_input";
-            this.Clean_input.Size = new System.Drawing.Size(113, 73);
+            this.Clean_input.Size = new System.Drawing.Size(170, 114);
             this.Clean_input.TabIndex = 12;
             this.Clean_input.Text = "Очистить поле";
             this.Clean_input.UseVisualStyleBackColor = true;
@@ -187,9 +200,10 @@ namespace WindowsFormsApp1
             // 
             this.Clean_output.Enabled = false;
             this.Clean_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Clean_output.Location = new System.Drawing.Point(866, 466);
+            this.Clean_output.Location = new System.Drawing.Point(1299, 728);
+            this.Clean_output.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Clean_output.Name = "Clean_output";
-            this.Clean_output.Size = new System.Drawing.Size(113, 73);
+            this.Clean_output.Size = new System.Drawing.Size(170, 114);
             this.Clean_output.TabIndex = 13;
             this.Clean_output.Text = "Очистить поле";
             this.Clean_output.UseVisualStyleBackColor = true;
@@ -197,10 +211,10 @@ namespace WindowsFormsApp1
             // 
             // Shifr
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.ClientSize = new System.Drawing.Size(1473, 1177);
             this.Controls.Add(this.Clean_output);
             this.Controls.Add(this.Clean_input);
             this.Controls.Add(this.Shifr5);
@@ -216,6 +230,7 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.Input_label);
             this.Controls.Add(this.choice);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Shifr";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Incoder/Decoder";
--- Групповой проект/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Групповой проект/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -54,7 +54,54 @@ namespace WindowsFormsApp1
             }
             if (Shifr5.Checked)
             {
-               
+                string inp = this.Input.Text;
+                string polibish = "ABCDEFGHIJK" +
+                                  "LMNOPQRSTUV" +
+                                  "WXYZabcdefg" +
+                                  "hijklmnopqr" +
+                                  "stuvwxyzАБВ" +
+                                  "ГДЕЁЖЗИЙКЛМ" +
+                                  "НОПРСТУФХЦЧ" +
+                                  "ШЩЪЫЬЭЮЯабв" +
+                                  "гдеёжзийклм" +
+                                  "нопрстуфхцч" +
+                                  "шщъыьэюя ";
+                int pos_x = 0, pos_y = 0;
+                int j = 0;
+                string outp = "";
+                string chast = "";
+                for (int i = 0; i < inp.Length; i++)
+                {
+                    j = polibish.IndexOf(inp[i]);
+                    if (j == -1)
+                    {
+                        outp += inp[i] + " ";
+                    }
+                    else
+                    {
+                        pos_x = j % 11;
+                        pos_y = j / 11;
+                        if (pos_x == 10)
+                        {
+                            chast += 'a';
+                        }
+                        else
+                        {
+                            chast += pos_x.ToString();
+                        }
+                        if (pos_y == 10)
+                        {
+                            chast += 'a';
+                        }
+                        else
+                        {
+                            chast += pos_y.ToString();
+                        }
+                        outp += chast + " ";
+                        chast = "";
+                    }
+                }
+                this.Output.Text = outp;
             }
         }
 
@@ -97,7 +144,52 @@ namespace WindowsFormsApp1
             }
             if (Shifr5.Checked)
             {
-                
+                string inp = this.Input.Text;
+                string polibish = "ABCDEFGHIJK" +
+                                  "LMNOPQRSTUV" +
+                                  "WXYZabcdefg" +
+                                  "hijklmnopqr" +
+                                  "stuvwxyzАБВ" +
+                                  "ГДЕЁЖЗИЙКЛМ" +
+                                  "НОПРСТУФХЦЧ" +
+                                  "ШЩЪЫЬЭЮЯабв" +
+                                  "гдеёжзийклм" +
+                                  "нопрстуфхцч" +
+                                  "шщъыьэюя ";
+                int j = 0;
+                int pos = 0;
+                string outp = "";
+                string[] mas = inp.Split(' ');
+                int pos_x = 0, pos_y = 0;
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (mas[i].Length != 2)
+                    {
+                        outp += mas[i];
+                    }
+                    else
+                    {
+                        if (mas[i][0] != 'a')
+                        {
+                            pos_x = int.Parse(mas[i][0].ToString());
+                        }
+                        else
+                        {
+                            pos_x = 10;
+                        }
+                        if (mas[i][1] != 'a')
+                        {
+                            pos_y = int.Parse(mas[i][1].ToString());
+                        }
+                        else
+                        {
+                            pos_y = 10;
+                        }
+                        pos = pos_y * 11 + pos_x;
+                        outp += polibish[pos];
+                    }
+                }
+                this.Output.Text = outp;
             }
         }
 
@@ -138,5 +230,6 @@ namespace WindowsFormsApp1
                 this.Clean_output.Enabled = false;
             }
         }
+
     }
 }
